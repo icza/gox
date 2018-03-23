a4fb24288d37efd30368701f64ae525f0dba336d
Andras Belicza <iczaaa@gmail.com>
2018-03-23 13:58:40 +0100

    Add missing NewInt16 and NewUint16 functions, add comprehensive testing

--- gox.go
+++ gox.go
@@ -55,6 +55,9 @@ func NewInt(i int) *int { return &i }
 // NewInt8 returns a pointer to the given int8 value.
 func NewInt8(i int8) *int8 { return &i }
 
+// NewInt16 returns a pointer to the given int16 value.
+func NewInt16(i int16) *int16 { return &i }
+
 // NewInt32 returns a pointer to the given int32 value.
 func NewInt32(i int32) *int32 { return &i }
 
@@ -62,10 +65,13 @@ func NewInt32(i int32) *int32 { return &i }
 func NewInt64(i int64) *int64 { return &i }
 
 // NewUint returns a pointer to the given uint value.
-func NewUint(i int) *int { return &i }
+func NewUint(i uint) *uint { return &i }
 
 // NewUint8 returns a pointer to the given uint8 value.
 func NewUint8(i uint8) *uint8 { return &i }
+
+// NewUint16 returns a pointer to the given uint16 value.
+func NewUint16(i uint16) *uint16 { return &i }
 
 // NewUint32 returns a pointer to the given uint32 value.
 func NewUint32(i uint32) *uint32 { return &i }
---
+++ gox_test.go
@@ -0,0 +1,102 @@
+package gox
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestMinMaxUint(t *testing.T) {
+	var max uint
+	max = ^max
+
+	if MaxUint != max {
+		t.Errorf("MaxUint is not max!")
+	}
+	if MinUint != max+1 {
+		t.Errorf("MinUint is not min!")
+	}
+}
+
+func TestMinMaxInt(t *testing.T) {
+	maxuint := uint(0)
+	maxuint = ^maxuint
+	max := int(maxuint >> 1)
+
+	if MaxInt != max {
+		t.Errorf("MaxInt is not max!")
+	}
+	if MinInt != -max-1 {
+		t.Errorf("MinInt is not min!")
+	}
+}
+
+func TestArchConsts(t *testing.T) {
+	arch32bit := reflect.TypeOf(0).Size() == 4
+
+	if arch32bit != Arch32bit {
+		t.Errorf("Arch32bit is incorrect!")
+	}
+	if arch32bit == Arch64bit {
+		t.Errorf("Arch64bit is incorrect!")
+	}
+}
+
+func TestPie(t *testing.T) {
+	Pie(nil)
+
+	var wasPanic bool
+	func() {
+		defer func() {
+			if x := recover(); x != nil {
+				wasPanic = true
+			}
+		}()
+		Pie(errors.New("test error"))
+	}()
+	if !wasPanic {
+		t.Errorf("Expected panic!")
+	}
+}
+
+func TestNewValues(t *testing.T) {
+	type is = []interface{}
+	cases := []struct {
+		name   string        // Name of the test case
+		f      interface{}   // Function to call
+		params []interface{} // Params to pass in subsequent calls
+	}{
+		{"NewBool", NewBool, is{false, true}},
+		{"NewString", NewString, is{"", "a"}},
+		{"NewInt", NewInt, is{0, 1}},
+		{"NewInt8", NewInt8, is{int8(0), int8(1)}},
+		{"NewInt16", NewInt16, is{int16(0), int16(1)}},
+		{"NewInt32", NewInt32, is{int32(0), int32(1)}},
+		{"NewInt64", NewInt64, is{int64(0), int64(1)}},
+		{"NewUint", NewUint, is{uint(0), uint(1)}},
+		{"NewUint8", NewUint8, is{uint8(0), uint8(1)}},
+		{"NewUint16", NewUint16, is{uint16(0), uint16(1)}},
+		{"NewUint32", NewUint32, is{uint32(0), uint32(1)}},
+		{"NewUint64", NewUint64, is{uint64(0), uint64(1)}},
+		{"NewFloat32", NewFloat32, is{float32(0), float32(1)}},
+		{"NewFloat64", NewFloat64, is{float64(0), float64(1)}},
+	}
+
+	for _, c := range cases {
+		fv := reflect.ValueOf(c.f)
+		for _, param := range c.params {
+			results := fv.Call([]reflect.Value{reflect.ValueOf(param)})
+			if len(results) != 1 {
+				t.Errorf("[%s(%v)] Expected 1 result(s), got: %d", c.name, param, len(results))
+			}
+			if got := results[0].Elem().Interface(); got != param {
+				t.Errorf("[%s(%v)] Expected: %v, got: %v", c.name, param, param, got)
+			}
+		}
+	}
+
+	// bp := NewBool(false)
+	// if bp == nil || *bp != false {
+	// 	t.Errorf("NewBool misbehaves!")
+	// }
+}
