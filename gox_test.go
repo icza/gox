ae56f3c8f829490ad319e882dd1b70ce8d711ec4
Andras Belicza <iczaaa@gmail.com>
2018-04-03 12:59:37 +0200

    Add test for IfXX functions and If.XX methods

--- gox_test.go
+++ gox_test.go
@@ -97,3 +97,64 @@ func TestNewValues(t *testing.T) {
 		}
 	}
 }
+
+func TestIfFuncs(t *testing.T) {
+	cases := []struct {
+		name string      // Name of the test case
+		f    interface{} // Function to call
+		a, b interface{} // a and b params to pass in
+	}{
+		{"IfIf", IfIf, "one", 2.0},
+		{"IfBool", IfBool, false, true},
+		{"IfString", IfString, "a", "b"},
+		{"IfInt", IfInt, 1, 2},
+		{"IfInt8", IfInt8, int8(1), int8(2)},
+		{"IfInt16", IfInt16, int16(1), int16(2)},
+		{"IfInt32", IfInt32, int32(1), int32(2)},
+		{"IfInt64", IfInt64, int64(1), int64(2)},
+		{"IfUint", IfUint, uint(1), uint(2)},
+		{"IfUint8", IfUint8, uint8(1), uint8(2)},
+		{"IfUint16", IfUint16, uint16(1), uint16(2)},
+		{"IfUint32", IfUint32, uint32(1), uint32(2)},
+		{"IfUint64", IfUint64, uint64(1), uint64(2)},
+		{"IfFloat32", IfFloat32, float32(1), float32(2)},
+		{"IfFloat64", IfFloat64, float64(1), float64(2)},
+		{"IfByte", IfByte, byte(1), byte(2)},
+		{"IfRune", IfRune, rune(1), rune(2)},
+	}
+
+	for _, c := range cases {
+		for _, cond := range []bool{false, true} {
+			// Test the "standalone" function and the similar method of If
+			for round := 0; round < 2; round++ {
+				var params []reflect.Value
+				var fv reflect.Value
+				switch round {
+				case 0: // "standalone" function
+					fv = reflect.ValueOf(c.f)
+					params = append(params, reflect.ValueOf(cond))
+				case 1: // method of If
+					fv = reflect.ValueOf(If(cond)).MethodByName(c.name[2:])
+				}
+				params = append(params, reflect.ValueOf(c.a), reflect.ValueOf(c.b))
+
+				results := fv.Call(params)
+				if len(results) != 1 {
+					t.Errorf("[%s(%v, %v, %v)] Expected 1 result(s), got: %d",
+						c.name, cond, c.a, c.b, len(results))
+				}
+
+				var exp interface{}
+				if cond {
+					exp = c.a
+				} else {
+					exp = c.b
+				}
+				if got := results[0].Interface(); got != exp {
+					t.Errorf("[%s(%v, %v, %v)] Expected: %v, got: %v",
+						c.name, cond, c.a, c.b, exp, got)
+				}
+			}
+		}
+	}
+}
