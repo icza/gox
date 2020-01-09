dcd04b0d8197414e10e17e94c5674a6be4b32b60
Andras Belicza <iczaaa@gmail.com>
2020-01-09 21:31:19 +0100

    Improve package example

--- example_test.go
+++ example_test.go
@@ -8,28 +8,32 @@ import (
 )
 
 func Example() {
-	b, one := NewBool(true), NewUint(1) // Pointers to non-zero values
-	fmt.Printf("*b: %t, *one: %d\n", *b, *one)
+	// Pointers to non-zero values
+	b, i, s := NewBool(true), NewUint(1), NewString("hi")
+	fmt.Printf("b: %t, i: %d, s: %s\n", *b, *i, *s)
 
+	// One way of mimicing the ternary operator:
 	for _, age := range []int{10, 20} {
 		state := If(age < 18).String("child", "adult")
-		fmt.Printf("Age:           %d, state: %s\n", age, state)
+		fmt.Printf("Age: %d, state: %s\n", age, state)
 	}
 
+	// And another one:
 	for _, tempC := range []int{-5, 10} {
 		fmt.Printf("Temperature: %d°C, state: %s\n",
 			tempC, IfString(tempC < 0, "solid", "liquid"))
 	}
 
+	// Quick "handling" of error:
 	n, err := strconv.Atoi("3")
 	Pie(err)
-	fmt.Println(n)
+	fmt.Println("Parsed:", n)
 
 	// Output:
-	// *b: true, *one: 1
-	// Age:           10, state: child
-	// Age:           20, state: adult
+	// b: true, i: 1, s: hi
+	// Age: 10, state: child
+	// Age: 20, state: adult
 	// Temperature: -5°C, state: solid
 	// Temperature: 10°C, state: liquid
-	// 3
+	// Parsed: 3
 }
