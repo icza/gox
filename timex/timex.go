320eaac7b17eabe3e32a22d540ead30ddc0c4431
Andras Belicza <iczaaa@gmail.com>
2020-01-09 16:27:43 +0100

    Add WeekStart() and example

--- timex/timex.go
+++ timex/timex.go
@@ -10,7 +10,7 @@ import "time"
 // Diff calculates the absolute difference between 2 time instanes in
 // years, months, days, hours, minutes and seconds.
 //
-// For more details, see https://stackoverflow.com/a/36531443/1705598
+// For details, see https://stackoverflow.com/a/36531443/1705598
 func Diff(a, b time.Time) (year, month, day, hour, min, sec int) {
 	if a.Location() != b.Location() {
 		b = b.In(a.Location())
@@ -56,4 +56,36 @@ func Diff(a, b time.Time) (year, month, day, hour, min, sec int) {
 	}
 
 	return
+}
+
+// WeekStart returns the time instant pointing to the start of the week given
+// by its year and ISO Week. Weeks are interpreted starting on Monday,
+// so the returned instant will be 00:00 of Monday of the designated week.
+//
+// One nice property of this function is that it handles out-of-range weeks nicely.
+// That is, if you pass 0 for the week, it will be interpreted as the last week
+// of the previous year. If you pass -1 for the week, it will designate
+// the second to last week of the previous year. Similarly, if you pass max week
+// of the year plus 1, it will be interpreted as the first week of the next year etc.
+//
+// This function only returns the given week's first day (Monday), because the
+// last day of the week is always its first day + 6 days.
+//
+// For details, see https://stackoverflow.com/a/52303730/1705598
+func WeekStart(year, week int) time.Time {
+	// Start from the middle of the year:
+	t := time.Date(year, 7, 1, 0, 0, 0, 0, time.UTC)
+
+	// Roll back to Monday:
+	if wd := t.Weekday(); wd == time.Sunday {
+		t = t.AddDate(0, 0, -6)
+	} else {
+		t = t.AddDate(0, 0, -int(wd)+1)
+	}
+
+	// Difference in weeks:
+	_, w := t.ISOWeek()
+	t = t.AddDate(0, 0, (week-w)*7)
+
+	return t
 }
--- timex/timex_test.go
+++ timex/timex_test.go
@@ -70,3 +70,31 @@ func TestDiff(t *testing.T) {
 		}
 	}
 }
+
+// ExampleWeekStart shows how to use the WeekStart() function.
+func ExampleWeekStart() {
+	inputs := []struct{ year, week int }{
+		{2018, -1},
+		{2018, 0},
+		{2018, 1},
+		{2018, 2},
+		{2019, 1},
+		{2019, 2},
+		{2019, 53},
+		{2019, 54},
+	}
+	for _, c := range inputs {
+		fmt.Printf("Week (%d,%2d) starts on: %v\n", c.year, c.week, WeekStart(c.year, c.week))
+
+	}
+
+	// Output:
+	// Week (2018,-1) starts on: 2017-12-18 00:00:00 +0000 UTC
+	// Week (2018, 0) starts on: 2017-12-25 00:00:00 +0000 UTC
+	// Week (2018, 1) starts on: 2018-01-01 00:00:00 +0000 UTC
+	// Week (2018, 2) starts on: 2018-01-08 00:00:00 +0000 UTC
+	// Week (2019, 1) starts on: 2018-12-31 00:00:00 +0000 UTC
+	// Week (2019, 2) starts on: 2019-01-07 00:00:00 +0000 UTC
+	// Week (2019,53) starts on: 2019-12-30 00:00:00 +0000 UTC
+	// Week (2019,54) starts on: 2020-01-06 00:00:00 +0000 UTC
+}
