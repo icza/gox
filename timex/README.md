a661a2bd1f08b5ab095af834b9d696360536133d
Andras Belicza <iczaaa@gmail.com>
2020-01-09 15:56:07 +0100

    Imrove docs

--- timex/README.md
+++ timex/README.md
@@ -2,4 +2,4 @@
 
 Package `timex` contains time- and time-calculation related utilities and helpers.
 
-The package uses only the standard `time` package.
+The package uses only the standard `time` and `strings` packages.
--- timex/timex.go
+++ timex/timex.go
@@ -1,7 +1,7 @@
 /*
 Package timex contains time- and time-calculation related utilities and helpers.
 
-The package uses only the standard `time` package.
+The package uses only the standard time and strings packages.
 */
 package timex
 
