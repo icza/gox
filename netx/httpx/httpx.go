07623a1441add8758a638866734f8ac20fe72ff8
Andras Belicza <iczaaa@gmail.com>
2020-03-17 17:35:48 +0100

    Add ShortenUserAgent and DecodeShortUA functions

--- netx/httpx/httpx.go
+++ netx/httpx/httpx.go
@@ -29,3 +29,82 @@ func ClientIP(r *http.Request) string {
 	// Last resort:
 	return r.RemoteAddr
 }
+
+var uaShortener = strings.NewReplacer(
+	"~", "~~", // This is so original ~ are preserved and can be decoded losslessly.
+	"Android", "~A",
+	"Chrome/", "~c",
+	"Edge/", "~e",
+	"Firefox/", "~f",
+	"Gecko/", "~g",
+	"(KHTML, like Gecko)", "~G",
+	"iPhone", "~i",
+	"Macintosh", "~I",
+	"AppleWebKit/", "~a",
+	"Linux", "~L",
+	"Mobile/", "~m",
+	"Mobile", "~M",
+	"Safari/", "~s",
+	"Version/", "~v",
+	"Windows", "~W",
+	"Mozilla/5.0 ", "~Z ", // This is to replace the prefix
+)
+
+// shortUADecoder is the inverse of uaShortener.
+var shortUADecoder = strings.NewReplacer(
+	"~~", "~",
+	"~A", "Android",
+	"~c", "Chrome/",
+	"~e", "Edge/",
+	"~f", "Firefox/",
+	"~g", "Gecko/",
+	"~G", "(KHTML, like Gecko)",
+	"~i", "iPhone",
+	"~I", "Macintosh",
+	"~a", "AppleWebKit/",
+	"~L", "Linux",
+	"~m", "Mobile/",
+	"~M", "Mobile",
+	"~s", "Safari/",
+	"~v", "Version/",
+	"~W", "Windows",
+	"~Z ", "Mozilla/5.0 ",
+)
+
+// ShortenUserAgent can be used to simplify, shorten user agent strings.
+// It replaces most frequent (and less informative) parts with short sequences.
+// The transformation is a bijection, the short form can be decoded back
+// into the original user agent string, see DecodeShortUA().
+//
+// Examples:
+//   -system: Chrome Generic Win10
+//        ua: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36
+//     short: ~Z (~W NT 10.0; Win64; x64) ~a537.36 ~G ~c80.0.3987.132 ~s537.36
+//
+//   -system: Firefox Generic Linux
+//        ua: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0
+//     short: ~Z (X11; Ubuntu; ~L x86_64; rv:73.0) ~g20100101 ~f73.0
+//
+//   -system: Safari 13.0 macOS
+//        ua: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15
+//     short: ~Z (~I; Intel Mac OS X 10_15_3) ~a605.1.15 ~G ~v13.0.5 ~s605.1.15
+//
+//   -system: Safari Apple iPhone XR
+//        ua: Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1
+//     short: ~Z (~i; CPU ~i OS 12_0 like Mac OS X) ~a605.1.15 ~G ~v12.0 ~m15E148 ~s604.1
+//
+//   -system: Samsung Galaxy S9
+//        ua: Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36
+//     short: ~Z (~L; ~A 8.0.0; SM-G960F Build/R16NW) ~a537.36 ~G ~c62.0.3202.84 ~M ~s537.36
+//
+// The goal is not to produce the shortest output, but to provide a reasonably
+// short output while maintaining readability.
+func ShortenUserAgent(ua string) string {
+	return uaShortener.Replace(ua)
+}
+
+// DecodeShortUA decodes the given shortened user agent string into its original form.
+// The shortUA input should be the output of ShortenUserAgent().
+func DecodeShortUA(shortUA string) string {
+	return shortUADecoder.Replace(shortUA)
+}
--- netx/httpx/httpx_test.go
+++ netx/httpx/httpx_test.go
@@ -33,3 +33,68 @@ func TestClientIP(t *testing.T) {
 		}
 	}
 }
+
+func TestUAShortening(t *testing.T) {
+	cases := []struct {
+		name    string
+		ua      string
+		shortUA string
+	}{
+		{"empty", "", ""},
+		{"decodability", "~m~~~A~", "~~m~~~~~~A~~"},
+		{
+			"Chrome Generic Win10",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+			"~Z (~W NT 10.0; Win64; x64) ~a537.36 ~G ~c80.0.3987.132 ~s537.36",
+		},
+		{
+			"Firefox Generic Win10",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0",
+			"~Z (~W NT 10.0; Win64; x64; rv:73.0) ~g20100101 ~f73.0",
+		},
+		{
+			"Safari 13.0 macOS",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15",
+			"~Z (~I; Intel Mac OS X 10_15_3) ~a605.1.15 ~G ~v13.0.5 ~s605.1.15",
+		},
+		{
+			"Chrome Generic macOS",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+			"~Z (~I; Intel Mac OS X 10_15_3) ~a537.36 ~G ~c80.0.3987.132 ~s537.36",
+		},
+		{
+			"Firefox Generic Linux",
+			"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0",
+			"~Z (X11; Ubuntu; ~L x86_64; rv:73.0) ~g20100101 ~f73.0",
+		},
+		{
+			"Edge 18.0 Win10",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362",
+			"~Z (~W NT 10.0; Win64; x64) ~a537.36 ~G ~c70.0.3538.102 ~s537.36 ~e18.18362",
+		},
+		{
+			"Opera Generic Win10",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.72",
+			"~Z (~W NT 10.0; Win64; x64) ~a537.36 ~G ~c79.0.3945.130 ~s537.36 OPR/66.0.3515.72",
+		},
+		{
+			"Safari Apple iPhone XR",
+			"Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1",
+			"~Z (~i; CPU ~i OS 12_0 like Mac OS X) ~a605.1.15 ~G ~v12.0 ~m15E148 ~s604.1",
+		},
+		{
+			"Samsung Galaxy S9",
+			"Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
+			"~Z (~L; ~A 8.0.0; SM-G960F Build/R16NW) ~a537.36 ~G ~c62.0.3202.84 ~M ~s537.36",
+		},
+	}
+
+	for _, c := range cases {
+		if got := ShortenUserAgent(c.ua); got != c.shortUA {
+			t.Errorf("[%s] Expected: %s, got: %s", c.name, c.shortUA, got)
+		}
+		if got := DecodeShortUA(c.shortUA); got != c.ua {
+			t.Errorf("[%s] Expected: %s, got: %s", c.name, c.ua, got)
+		}
+	}
+}
