72ca368753e850e4ed42848d45ad2068cf406a73
Andras Belicza <iczaaa@gmail.com>
2020-03-17 11:36:16 +0100

    Add link to SO question

--- stringsx/stringsx.go
+++ stringsx/stringsx.go
@@ -7,6 +7,8 @@ import (
 
 // Clean removes non-graphic characters from the given string.
 // Removable characters are the ones for which unicode.IsGraphic() returns false.
+//
+// For details, see https://stackoverflow.com/a/58994297/1705598
 func Clean(s string) string {
 	return strings.Map(func(r rune) rune {
 		if unicode.IsGraphic(r) {
