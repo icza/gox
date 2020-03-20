a6ff52ab3d90d8a6e93ca11201b52468686e3d39
Andras Belicza <iczaaa@gmail.com>
2020-03-20 18:45:35 +0100

    Add LimitRunes() function

--- stringsx/stringsx.go
+++ stringsx/stringsx.go
@@ -17,3 +17,24 @@ func Clean(s string) string {
 		return -1
 	}, s)
 }
+
+// LimitRunes returns a slice of s that contains at most the given number of runes.
+// If n is 0 or negative, the empty string is returned.
+// If s has more runes than n, s is returned.
+//
+// Each byte of invalid UTF-8 sequences count as one when counting the limit, e.g.
+//   LimitRunes("\xff\xffab", 3) // returns "\xff\xffa"
+func LimitRunes(s string, n int) string {
+	if n <= 0 || s == "" {
+		return ""
+	}
+
+	for i := range s {
+		n--
+		if n == -1 {
+			return s[:i]
+		}
+	}
+	// s has n or more runes
+	return s
+}
--- stringsx/stringsx_test.go
+++ stringsx/stringsx_test.go
@@ -23,3 +23,41 @@ func TestClean(t *testing.T) {
 		}
 	}
 }
+
+func TestLimitRunes(t *testing.T) {
+	cases := []struct {
+		name string
+		s    string
+		n    int
+		exp  string
+	}{
+		{"empty", "", 0, ""},
+		{"empty-2", "", 10, ""},
+		{"normal", "abc", 0, ""},
+		{"normal-2", "abc", 2, "ab"},
+		{"normal-3", "abc", 3, "abc"},
+		{"normal-3", "abc", 20, "abc"},
+		{"unicode", "世界世界", 0, ""},
+		{"unicode-2", "世界世界", 2, "世界"},
+		{"unicode-3", "世界世界", 20, "世界世界"},
+		{"invalid-utf-8", "\xff", 0, ""},
+		{"invalid-utf-8-2", "\xff", 1, "\xff"},
+		{"invalid-utf-8-3", "\xff", 2, "\xff"},
+		{"invalid-utf-8#2", "a\xffb", 0, ""},
+		{"invalid-utf-8#2-2", "a\xffb", 1, "a"},
+		{"invalid-utf-8#2-3", "a\xffb", 2, "a\xff"},
+		{"invalid-utf-8#2-4", "a\xffb", 3, "a\xffb"},
+		{"invalid-utf-8#2-5", "a\xffb", 4, "a\xffb"},
+		{"invalid-utf-8#3", "\xff\xffb", 0, ""},
+		{"invalid-utf-8#3-2", "\xff\xffb", 1, "\xff"},
+		{"invalid-utf-8#3-3", "\xff\xffb", 2, "\xff\xff"},
+		{"invalid-utf-8#3-4", "\xff\xffb", 3, "\xff\xffb"},
+		{"invalid-utf-8#3-5", "\xff\xffb", 4, "\xff\xffb"},
+	}
+
+	for _, c := range cases {
+		if got := LimitRunes(c.s, c.n); got != c.exp {
+			t.Errorf("[%s] Expected: %s, got: %s", c.name, c.exp, got)
+		}
+	}
+}
