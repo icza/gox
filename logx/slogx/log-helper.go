591faca15f12bc2659cb5b1cbc204c07bdd10fa4
Andras Belicza <iczaaa@gmail.com>
2025-12-29 20:01:07 +0100

    Improve LogHelper to log the proper caller function of LogHelper

--- logx/slogx/log-helper.go
+++ logx/slogx/log-helper.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"runtime"
+	"time"
 )
 
 // LogHelper wraps a slog.Logger and provides helper methods like formatted logging.
@@ -12,45 +14,66 @@ type LogHelper struct {
 	*slog.Logger
 }
 
+// log logs a message at the given level.
+// It retrieves the caller information to include in the log record, so the caller of LogHelper will be included in logs (not the called LogHelper method).
+// "Designed" to be called by other LogHelper methods like Fatal.
+func (lh LogHelper) log(level slog.Level, msg string, args ...any) {
+	if !lh.Enabled(context.Background(), level) {
+		return
+	}
+
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // skip Callers + log + caller LogHelper method
+	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
+	r.Add(args...)
+
+	_ = lh.Handler().Handle(context.Background(), r)
+}
+
+// logf logs a formatted message at the given level.
+// It retrieves the caller information to include in the log record, so the caller of LogHelper will be included in logs (not the called LogHelper method).
+// "Designed" to be called by other LogHelper methods like Warnf.
+func (lh LogHelper) logf(level slog.Level, format string, args ...any) {
+	if !lh.Enabled(context.Background(), level) {
+		return
+	}
+
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // skip Callers + logf + caller LogHelper method
+	r := slog.NewRecord(time.Now(), level, fmt.Sprintf(format, args...), pcs[0])
+
+	_ = lh.Handler().Handle(context.Background(), r)
+}
+
 // Debugf logs a formatted debug message.
 func (lh LogHelper) Debugf(format string, a ...any) {
-	if lh.Enabled(context.Background(), slog.LevelDebug) {
-		lh.Debug(fmt.Sprintf(format, a...))
-	}
+	lh.logf(slog.LevelDebug, format, a...)
 }
 
 // Infof logs a formatted info message.
 func (lh LogHelper) Infof(format string, a ...any) {
-	if lh.Enabled(context.Background(), slog.LevelInfo) {
-		lh.Info(fmt.Sprintf(format, a...))
-	}
+	lh.logf(slog.LevelInfo, format, a...)
 }
 
 // Warnf logs a formatted warning message.
 func (lh LogHelper) Warnf(format string, a ...any) {
-	if lh.Enabled(context.Background(), slog.LevelWarn) {
-		lh.Warn(fmt.Sprintf(format, a...))
-	}
+	lh.logf(slog.LevelWarn, format, a...)
 }
 
 // Errorf logs a formatted error message.
 func (lh LogHelper) Errorf(format string, a ...any) {
-	if lh.Enabled(context.Background(), slog.LevelError) {
-		lh.Error(fmt.Sprintf(format, a...))
-	}
+	lh.logf(slog.LevelError, format, a...)
 }
 
 // Fatalf logs a formatted error message and calls [os.Exit](1).
 func (lh LogHelper) Fatalf(format string, a ...any) {
-	if lh.Enabled(context.Background(), slog.LevelError) {
-		lh.Error(fmt.Sprintf(format, a...))
-	}
+	lh.logf(slog.LevelError, format, a...)
 	os.Exit(1)
 }
 
 // Fatal is equivalent to [log/slog.Logger.Error] followed by [os.Exit](1).
 func (lh LogHelper) Fatal(msg string, args ...any) {
-	lh.Error(msg, args...)
+	lh.log(slog.LevelError, msg, args...)
 	os.Exit(1)
 }
 
